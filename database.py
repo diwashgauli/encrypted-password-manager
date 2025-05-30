c8ad3401263a8b41eac2299f5d140631e9e69164
Diwas Gauli <gaulidiwas@gmail.com>
2025-05-30 15:08:22 +0545

    added functionality to get password by decrypting it and list out servies

--- database.py
+++ database.py
@@ -10,8 +10,24 @@ def connect():
     ''')
     return conn
 
-def save_password(app_name, encrypted_password):
+def save_password(service, encrypted_password):
     conn = connect()
-    conn.execute("REPLACE INTO passwords (service, password) VALUES (?, ?)", (app_name, encrypted_password))
+    conn.execute("REPLACE INTO passwords (service, password) VALUES (?, ?)", (service, encrypted_password))
     conn.commit()
     conn.close()
+
+def fetch_password(service):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SELECT password FROM passwords WHERE service=?", (service,))
+    row = cur.fetchone()
+    conn.close()
+    return row[0] if row else None
+
+def list_services():
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SELECT service FROM passwords")
+    rows = cur.fetchall()
+    conn.close()
+    return [r[0] for r in rows]
--- main.py
+++ main.py
@@ -5,20 +5,38 @@ import database
 fernet = get_encryptor()
 
 def add_password():
-    app_name = input("Application Name: ")
+    service = input("Service name: ")
     password = input("Password: ")
     encrypted = fernet.encrypt(password.encode())
-    database.save_password(app_name, encrypted.decode())
+    database.save_password(service, encrypted.decode())
     print("Saved.")
 
+def get_password():
+    service = input("Service name: ")
+    data = database.fetch_password(service)
+    if data:
+        decrypted = fernet.decrypt(data.encode()).decode()
+        print("Password:", decrypted)
+    else:
+        print("Not found.")
+
+def show_services():
+    services = database.list_services()
+    print("Saved services:")
+    for s in services:
+        print("-", s)
 
 def main():
     while True:
-        print("\n1. Add Password\n2. Exit")
+        print("\n1. Add Password\n2. Get Password\n3. List Services\n4. Exit")
         choice = input("Choose: ")
         if choice == "1":
             add_password()
         elif choice == "2":
+            get_password()
+        elif choice == "3":
+            show_services()
+        elif choice == "4":
             break
         else:
             print("Invalid")
